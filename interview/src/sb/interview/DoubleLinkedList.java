75de127d992e74d9c984a5c64cd8ac4fd177130a
skybutter <skybutter@gmail.com>
2016-05-20 14:59:12 -0700

    added prime number method

--- interview/src/sb/interview/DoubleLinkedList.java
+++ interview/src/sb/interview/DoubleLinkedList.java
@@ -101,4 +101,23 @@ public class DoubleLinkedList<T> {
  		String temp = sb.substring(0, sb.length()-1);
 		return "size=" + size + ":" + temp + "]";
 	}
+	
+	public static void main(String[] arg) {
+		DoubleLinkedList<Integer> list = new DoubleLinkedList<Integer>();
+		for (int i=0; i < 10; i++) {
+			list.add(i);
+		}
+		System.out.println(list);
+		for (int i=0; i < 2; i++) {
+			list.removeLast();
+		}
+		System.out.println(list);
+		for (int i=0; i < 2; i++) {
+			list.removeFirst();
+		}
+		System.out.println(list);
+		list.add(2, 99);
+		System.out.println(list);
+	}
+
 }
