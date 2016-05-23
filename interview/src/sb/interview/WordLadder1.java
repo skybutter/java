b12ac0efc687313401e41882b57053fec6ba4e82
skybutter <skybutter@gmail.com>
2016-05-23 12:18:01 -0700

    added explanation

--- interview/src/sb/interview/WordLadder1.java
+++ interview/src/sb/interview/WordLadder1.java
@@ -23,6 +23,26 @@ import java.util.Set;
  * see http://www.programcreek.com/2012/12/leetcode-word-ladder/
  * @author alan
  *
+ * http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1134/handouts/15-Assignment2.pdf
+ * 
+ * For word ladders, the breadth-first strategy starts by examining those ladders that are
+ * one step away from the original word, which means that only one letter has been
+ * changed. If any of these single-step changes reach the destination word, you’re done. If
+ * not, you can then move on to check all ladders that are two steps away from the original,
+ * which means that two letters have been changed. In computer science, each step in such
+ * a process is called a hop.
+ * The breadth-first algorithm is typically implemented by using a queue to store partial
+ * ladders that represent possibilities to explore. The ladders are enqueued in order of
+ * increasing length. The first elements enqueued are all the one-hop ladders, followed by
+ * the two-hop ladders, and so on. Because queues guarantee first-in/first-out processing,
+ * these partial word ladders will be dequeued in order of increasing length.
+ * To get the process started, you simply add a ladder consisting of only the start word to
+ * the queue. From then on, the algorithm operates by dequeueing the ladder from the front
+ * of the queue and determining whether it ends at the goal. If it does, you have a complete
+ * ladder, which must be minimal. If not, you take that partial ladder and extend it to reach
+ * words that are one additional hop away, and enqueue those extended ladders, where they
+ * will be examined later. If you exhaust the queue of possibilities without having found a
+ * completed ladder, you can conclude that no ladder exists.
  */
 public class WordLadder1 {
 
